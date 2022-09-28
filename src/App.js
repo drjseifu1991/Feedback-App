a16d1153efcbad7ffb55d8a839725b19fdaced89
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2022-09-28 16:37:12 +0300

    After first commit

---
+++ src/App.js
@@ -0,0 +1,4 @@
+const App = () => {
+    return <h1>Hello this is from APP JS</h1>
+}
+export default App
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+
+// ReactDOM.render(<h1>My Feedback App</h1>,document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
